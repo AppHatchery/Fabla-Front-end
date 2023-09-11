9972029e291d6aa1203af8f78d4d9b268810e668
yago1994 <yagoaral@hotmail.com>
2023-09-11 12:33:16 -0400

    added copyright;

--- lib/screens/settings/presentation/settings.dart
+++ lib/screens/settings/presentation/settings.dart
@@ -176,7 +176,14 @@ class _SettingsState extends State<Settings> {
                 "Dayrio Version 1.1",
                 style: CustomTypography()
                     .bodyMedium(color: CustomColors.textSecondaryContent),
-              )
+              ),
+              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                Text(
+                  "Copyright © 2023 Emory University",
+                  style: CustomTypography()
+                      .bodyMedium(color: CustomColors.textSecondaryContent),
+                ),
+              ]),
             ],
           ),
         ),
