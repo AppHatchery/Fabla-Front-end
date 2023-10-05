5fdf430866b799076f61fe59fb3ce13528b89b0d
Wiza-Munthali <42870528+Wiza-Munthali@users.noreply.github.com>
2023-10-05 21:54:36 +0200

    Pendo (#108)
    * Add Pendo
    
    1. Added Pendo Service
    2. Configured Android Pendo
    3. Configured iOS Pendo
    4. Added folder called secrets in the core directory
    
    * Update .gitignore
    
    Keeping API keys away from Git

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,11 @@ import 'package:audio_diaries_flutter/screens/diary/presentation/cubit/diary/dia
 import 'package:audio_diaries_flutter/screens/diary/presentation/cubit/diary/summary_cubit.dart';
 import 'package:audio_diaries_flutter/screens/diary/presentation/pages/new_diary.dart';
 import 'package:audio_diaries_flutter/screens/home/presentation/pages/homepage.dart';
+import 'package:audio_diaries_flutter/screens/onboarding/domain/repository/setup_repository.dart';
 import 'package:audio_diaries_flutter/screens/onboarding/presentation/cubit/login/login_cubit.dart';
 import 'package:audio_diaries_flutter/screens/onboarding/presentation/cubit/setup/setup_cubit.dart';
 import 'package:audio_diaries_flutter/screens/settings/presentation/settings.dart';
+import 'package:audio_diaries_flutter/services/pendo_service.dart';
 import 'package:audio_diaries_flutter/services/route_service.dart';
 import 'package:audio_diaries_flutter/theme/custom_colors.dart';
 import 'package:audio_diaries_flutter/theme/custom_icons.dart';
@@ -29,6 +31,7 @@ void main() async {
   objectbox = await ObjectBox.create();
   await configureAmplify();
   await NotificationService.init();
+  await PendoService.init();
   final route = await RouteService().getRoute();
   runApp(MyApp(
     route: route,
@@ -131,6 +134,7 @@ class _HubState extends State<Hub> with SingleTickerProviderStateMixin {
   @override
   void initState() {
     tabController = TabController(length: pages.length, vsync: this);
+    startPendo();
     super.initState();
   }
 
@@ -157,6 +161,12 @@ class _HubState extends State<Hub> with SingleTickerProviderStateMixin {
         ),
       ),
     );
+  }
+
+  startPendo() async {
+    final repository = SetupRepository();
+    final participant = repository.getParticipant();
+    await PendoService.start(participant!.studyCode.toString());
   }
 
   static const navigationBars = <Tab>[
