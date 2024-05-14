fe687e156f3773cba3f8e99203713fff0c666423
yago1994 <yagoaral@hotmail.com>
2024-05-14 17:26:15 -0400

    adjusted time format to -;

--- lib/core/network/upload.dart
+++ lib/core/network/upload.dart
@@ -64,7 +64,7 @@ Future<bool> upload(String studyCode, DiaryModel diary) async {
             
 
             DateTime now = DateTime.now();
-            String formattedTime = DateFormat('HH:mm:ss').format(now);
+            String formattedTime = DateFormat('HH-mm-ss').format(now);
             var filename = "${studyCode}_${formatSubmissionDate(diary.start)}_$formattedTime.mp3";
             var awsPath = "$studyCode/$date/prompt_$promptNumber/$filename";
             audioData
