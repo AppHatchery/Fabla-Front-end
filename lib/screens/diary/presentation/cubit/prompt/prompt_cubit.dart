07cfde09862322ccb3f6f38959de8f81f45b44e1
Naomi Nyama <nyamanaomi38@gmail.com>
2024-08-06 08:41:18 +0200

    -added show widget implementation

--- lib/screens/diary/presentation/cubit/prompt/prompt_cubit.dart
+++ lib/screens/diary/presentation/cubit/prompt/prompt_cubit.dart
@@ -33,7 +33,27 @@ class PromptCubit extends Cubit<PromptState> {
   
     Future<void> loadPrompt(DiaryModel diary, PromptModel prompt) async {
       bool showWidget = false;
+        final today = DateTime.now();
+        final start = DateTime(today.year, today.month, today.day, 0, 0, 0);
+
+      String? studyCode = setupRepository.getParticipant()?.studyCode;
+
+        final source =
+            await PreferenceService().getStringPreference(key: 'futureDate');
+        final date = source ?? start;
+        final futureDate = DateTime.parse(date.toString());
+
     try {
+      if (int.parse(studyCode!) % 2 == 0 && today.isBefore(futureDate)) {
+        showWidget = true;
+      } else if (int.parse(studyCode) % 2 == 0 && today.isAfter(futureDate)) {
+        showWidget = false;
+      } else if (int.parse(studyCode) % 2 != 0 && today.isBefore(futureDate)) {
+        showWidget = false;
+      } else if (int.parse(studyCode) % 2 != 0 && today.isAfter(futureDate)) {
+        showWidget = true;
+      }
+
       emit(PromptLoading(prompt));
       final newPrompt = _repository.load(diary, prompt.id);
       await Future.delayed(const Duration(microseconds: 1));
