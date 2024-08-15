0d2e45a1bd8ba5a98ce548ff007e217e27cf82d5
Wiza-Munthali <wizamunthali@gmail.com>
2024-08-15 17:29:57 +0200

    Update incentive.dart

--- lib/screens/home/data/incentive.dart
+++ lib/screens/home/data/incentive.dart
@@ -12,8 +12,8 @@ class Incentive {
 
   factory Incentive.fromJson(Map<String, dynamic> json) {
     return Incentive(
-      amount: json['amount'],
-      bonus: json['bonus'],
+      amount: double.parse(json['amount']),
+      bonus: double.parse(json['bonus']),
       currency: json['currency'] ?? '\$',
       threshold: json['threshold'],
     );
@@ -21,8 +21,8 @@ class Incentive {
 
   Map<String, dynamic> toJson() {
     return {
-      'amount': amount,
-      'bonus': bonus,
+      'amount': amount.toString(),
+      'bonus': bonus.toString(),
       'currency': currency,
       'threshold': threshold,
     };
