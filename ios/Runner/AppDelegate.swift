8e2631967574ff8a2122881374f42cb1065d8aff
kennedy <kennedylinzie@gmail.com>
2024-12-11 09:21:34 +0200

    apple related

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,7 +2,7 @@ import UIKit
 import Flutter
 import Pendo
 
-@UIApplicationMain
+@main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
