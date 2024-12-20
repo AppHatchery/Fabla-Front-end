5418fdc04b8fd335b1a56d633108a11831d4ab23
Wiza-Munthali <wizamunthali@gmail.com>
2024-12-20 10:38:23 +0200

    Update login_repository.dart
    Default to empty list if no permissions

--- lib/screens/onboarding/domain/repository/login_repository.dart
+++ lib/screens/onboarding/domain/repository/login_repository.dart
@@ -164,9 +164,9 @@ class LoginRepository {
           final setup = SetupRepository();
           final questions = data['onboarding_questions'] as List;
 
-          final permissions = (data['permissions'] as List)
-              .map((permission) => (permission as String).toLowerCase())
-              .toList();
+          final permissions = (data['permissions'] as List?)
+              ?.map((permission) => (permission as String).toLowerCase())
+              .toList() ?? [];
           // Save the extra permissions to the shared preferences
           await PreferenceService().setStringListPreference(
             key: 'extra_permissions',
