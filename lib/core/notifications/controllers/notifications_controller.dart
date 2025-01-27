94b234b527b4d710b6ae3e5d3c71879556ddbf5a
kennedy <kennedylinzie@gmail.com>
2025-01-27 08:21:02 +0200

    Push notifications (#486)
    * PUSH NOTIFICATION
    
    * apple related
    
    * more changes
    
    * update
    
    * Update AppDelegate.swift
    
    * ios
    
    * last one for today
    
    * few modifications
    
    * test
    
    * working system
    
    * remove pinpoint
    
    * ios changes
    
    * Update AppDelegate.swift
    
    * clean up

---
+++ lib/core/notifications/controllers/notifications_controller.dart
@@ -0,0 +1,122 @@
+import 'dart:developer';
+import 'dart:io';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
+
+class NotificationsController {
+  late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+
+  Future<void> initialize() async {
+    await FirebaseMessaging.instance.getInitialMessage();
+   // await requestPermission();
+    await setupNotificationPlugin();
+    //await getToken();
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage event) {
+      messageHandler(event);
+    });
+  }
+
+  Future<void> messageHandler(RemoteMessage message) async {
+    final notification = message.notification;
+    final androidNotification = notification?.android;
+
+    if (notification != null && androidNotification != null) {
+      String? imagePath;
+      StyleInformation notificationStyle = const DefaultStyleInformation(true, true);
+
+      if (androidNotification.imageUrl != null &&
+          androidNotification.imageUrl!.isNotEmpty) {
+        imagePath = await _downloadAndSaveFile(androidNotification.imageUrl!, 'bigPicture');
+
+        notificationStyle = BigPictureStyleInformation(
+          FilePathAndroidBitmap(imagePath),
+          contentTitle: '<b>${notification.title}</b>',
+          htmlFormatContentTitle: true,
+          summaryText: '${notification.body}',
+          htmlFormatSummaryText: true,
+        );
+      }
+
+      await flutterLocalNotificationsPlugin.show(
+        message.hashCode,
+        notification.title,
+        notification.body,
+        NotificationDetails(
+          android: AndroidNotificationDetails(
+            'high_importance_channel', // Channel ID
+            'High Importance Notifications', // Channel Name
+            channelDescription: 'This channel is used for important notifications.',
+            importance: Importance.max,
+            styleInformation: notificationStyle,
+            largeIcon: imagePath != null ? FilePathAndroidBitmap(imagePath) : null,
+          ),
+        ),
+      );
+    }
+  }
+
+  Future<void> requestPermission() async {
+    FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+    NotificationSettings settings = await messaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: true,
+      provisional: false,
+      sound: true,
+    );
+
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      messaging.setForegroundNotificationPresentationOptions(
+        alert: true,
+        badge: true,
+        sound: true,
+      );
+    }
+  }
+
+  Future<void> getToken() async {
+    final token = await FirebaseMessaging.instance.getToken();
+    log("Firebase Token: $token");
+  }
+
+  Future<void> setupNotificationPlugin() async {
+    flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('@mipmap/ic_launcher');
+    final DarwinInitializationSettings initializationSettingsDarwin =
+        DarwinInitializationSettings(
+      requestSoundPermission: false,
+      requestBadgePermission: false,
+      requestAlertPermission: false,
+    );
+
+    final InitializationSettings initializationSettings =
+        InitializationSettings(
+            android: initializationSettingsAndroid,
+            iOS: initializationSettingsDarwin);
+    await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+        onDidReceiveNotificationResponse: onDidReceiveNotificationResponse);
+  }
+
+  void onDidReceiveNotificationResponse(NotificationResponse response) async {
+    if (response.payload != null) {
+      log('Notification payload: ${response.payload}');
+    }
+  }
+
+  Future<String> _downloadAndSaveFile(String url, String fileName) async {
+    final Directory directory = await getApplicationDocumentsDirectory();
+    final String filePath = '${directory.path}/$fileName';
+    final http.Response response = await http.get(Uri.parse(url));
+    final File file = File(filePath);
+    await file.writeAsBytes(response.bodyBytes);
+    return filePath;
+  }
+}
--- lib/screens/onboarding/domain/repository/setup_repository.dart
+++ lib/screens/onboarding/domain/repository/setup_repository.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:io';
 import 'dart:math';
 import 'dart:developer' as dev;
+import 'dart:io';
 // import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:audio_diaries_flutter/core/database/dao/experiment_dao.dart';
 import 'package:audio_diaries_flutter/core/database/dao/protocal_dao.dart';
@@ -37,6 +38,7 @@ import '../../../../objectbox.g.dart';
 import '../../../diary/data/protocol.dart';
 import '../../../diary/domain/entities/protocol_entity.dart';
 import '../entities/participant.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 class SetupRepository {
   final ParticipantDAO _participantDAO =
@@ -505,11 +507,40 @@ class SetupRepository {
       extras[question.variable] = question.answer;
     }
 
+  String? firebaseToken;
+  try {
+    firebaseToken = await FirebaseMessaging.instance.getToken();
+    if (firebaseToken != null) {
+      debugPrint("Firebase Token: $firebaseToken");
+    } else {
+      debugPrint("Failed to fetch Firebase token: Token is null.");
+    }
+  } catch (e) {
+    debugPrint("Error fetching Firebase token: $e");
+  }
+
+  String platformName;
+  if (Platform.isAndroid) {
+    platformName = 'GCM';
+    debugPrint("Running on Android platform.");
+  } else if (Platform.isIOS) {
+    platformName = 'APNS';
+    debugPrint("Running on iOS platform.");
+  } else {
+    platformName = 'Unsupported';
+    debugPrint("Running on an unsupported platform.");
+  }
+
+  // Log the platform-specific name (optional)
+  debugPrint("Firebase Platform Name: $platformName");
+
     map.addAll(
       {
         'participant_id': participant!.studyCode.toString(),
         'login_code': experiment!.login,
         'extras': jsonEncode(extras),
+        'token': firebaseToken,
+        'service': platformName,
       },
     );
 
