d7806b667728f439ffceb73ba73c485c1c81ffe8
Wiza-Munthali <wizamunthali@gmail.com>
2025-05-13 17:05:22 +0200

    Update notification_access.dart
    Allow user to go through even without the notification access allowed

--- lib/screens/onboarding/presentation/pages/notification_access.dart
+++ lib/screens/onboarding/presentation/pages/notification_access.dart
@@ -289,15 +289,13 @@ class _NotificationAccessPageState extends State<NotificationAccessPage>
     if (mounted) setState(() => granted = results.isGranted);
 
     checkBattery();
-    if (results.isGranted) {
-      if (requested) {
-        await PreferenceService()
-            .setBoolPreference(key: 'notification_requested', value: true);
-        track(timer.stop(), "Finished");
-        if (context.mounted) {
-          RouteService()
-              .navigate(null, context: context, current: 'notification_access');
-        }
+    if (requested) {
+      await PreferenceService()
+          .setBoolPreference(key: 'notification_requested', value: true);
+      track(timer.stop(), "Finished");
+      if (context.mounted) {
+        RouteService()
+            .navigate(null, context: context, current: 'notification_access');
       }
     }
 
