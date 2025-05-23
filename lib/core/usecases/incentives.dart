024f3b07875fe9766347cd563b4a4178cce52682
Wiza-Munthali <wizamunthali@gmail.com>
2025-05-23 09:17:17 +0200

    Bug Fixes
    1. Adding incentives to AWS Submissions
    2. Making references for all media types and not only the audio questions
    3. Updated version in YAML
    4. Include Subtitle in free response questions

---
+++ lib/core/usecases/incentives.dart
@@ -0,0 +1,72 @@
+import 'package:audio_diaries_flutter/core/network/upload.dart';
+import 'package:audio_diaries_flutter/core/utils/formatter.dart';
+import 'package:audio_diaries_flutter/core/utils/statuses.dart';
+import 'package:audio_diaries_flutter/screens/diary/data/diary.dart';
+import 'package:audio_diaries_flutter/screens/diary/domain/repository/diary_repository.dart';
+import 'package:audio_diaries_flutter/screens/home/data/study.dart';
+import 'package:audio_diaries_flutter/screens/onboarding/domain/repository/setup_repository.dart';
+
+import 'dart:developer' as dev;
+
+/// Function to calculate earned incentives based on diary submissions and completion rates.
+/// This function is triggered after a submission of entries
+///
+/// Returns a `bool` value after the calculated incentives are submitted to AWS.
+Future<bool> calculateEarnedIncentivesForAWS(
+    {required String participantID}) async {
+  final repository = SetupRepository();
+  final experiment = repository.getExperiment();
+  final experimentCode = experiment.login;
+  final diaryRepository = DiaryRepository();
+
+  // Get the all diaries with multiple entries
+  final diaries = diaryRepository.getAllDiariesWithMultipleEntries();
+
+  final studies = diaryRepository.getAllStudies();
+
+  // Make a map of study to diaries
+  // where the studyID is equal to the diary's studyID
+  final Map<StudyModel, List<DiaryModel>> studyDiariesMap = {};
+  for (var study in studies) {
+    studyDiariesMap[study] =
+        diaries.where((d) => d.studyID == study.studyId).toList();
+  }
+
+  double earned = 0;
+  String? currency = studies.firstOrNull?.incentive.currency;
+
+  studyDiariesMap.forEach((study, diaries) {
+    int completedCount = 0;
+
+    // Check if the diary is submitted
+    // and add the incentive amount to the earned amount
+    for (var diary in diaries) {
+      if (diary.status == DiaryStatus.submitted) {
+        earned += study.incentive.amount;
+        completedCount++;
+      }
+    }
+
+    // Check if the completion rate is greater than or equal to the threshold
+    // and add the bonus amount to the earned amount
+    double completionRate = completedCount / diaries.length;
+    if (completionRate * 100 >= study.incentive.threshold) {
+      earned += study.incentive.bonus;
+    }
+  });
+
+  dev.log("Earned: $earned", name: "Incentives - submitDiary");
+
+  final entry = PromptEntry(
+    participantID: participantID,
+    experimentCode: experimentCode,
+    questionTitle: "Incentives Earned",
+    diaryID: '',
+    promptID: '',
+    response: formatMoney(earned, currency: currency),
+    questionsType: "Incentives",
+    required: true,
+  );
+
+  return await uploadNonAudioData([entry]);
+}
