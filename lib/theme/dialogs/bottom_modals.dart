73441b6a2c33d8b49fa5c3f43585adf3950ad9db
Mabuchi Nyirenda <79569415+BuchiNy@users.noreply.github.com>
2025-10-31 08:33:11 +0200

    710 webview internet connectivity (#712)
    * no network warning for the webview
    
    * added functionality for the try again button
    
    * tidy up
    
    * moved error display to the custom webview from the bottom modal file
    
    * removed unused imports from the bottom modal file.
    
    * tidy ups: change colors, removed controler.reload from dispose

--- lib/theme/dialogs/bottom_modals.dart
+++ lib/theme/dialogs/bottom_modals.dart
@@ -17,7 +17,6 @@ import 'package:audio_session/audio_session.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_sound/public/flutter_sound_recorder.dart';
 import 'package:gradient_borders/box_borders/gradient_box_border.dart';
 import 'package:path_provider/path_provider.dart';
