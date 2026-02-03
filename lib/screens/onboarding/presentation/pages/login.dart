f484a867a52ea260ab15ee24c5fb642ab3c87c4b
BuchiNY <mabuchi.mn@gmail.com>
2026-02-03 20:14:30 +0200

    validation for empty text fields added

--- lib/screens/onboarding/presentation/pages/login.dart
+++ lib/screens/onboarding/presentation/pages/login.dart
@@ -250,15 +250,19 @@ class _LoginPageState extends State<LoginPage> with WidgetsBindingObserver {
   }
 
   void login() {
-    if (controller.text.isNotEmpty) {
       final lastNonSpaceIndex = controller.text.lastIndexOf(RegExp(r'[^ ]'));
       final text = controller.text.substring(0, lastNonSpaceIndex + 1);
       final code = text;
 
       if (code.isNotEmpty) {
         loginCubit.login(code);
-      } else {}
-    }
+      } else {
+        setState(() {
+          error = true;
+          message = 'Participant ID is required';
+        });
+        return;
+      }
   }
 
   track(int spent, String status) async {
