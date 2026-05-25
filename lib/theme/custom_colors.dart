9ac57bc3f4481be87159e2fa926366785e9de0c6
carol-kadza <carolinekadzakalowa02@gmail.com>
2026-05-25 10:35:51 +0200

    fixed confirm button color

--- lib/theme/custom_colors.dart
+++ lib/theme/custom_colors.dart
@@ -7,6 +7,8 @@ class CustomColors {
   static const Color productLightPrimaryActive = Color(0xFFB4D5FF);
   static const Color productLightSecondaryNormal = Color(0xFF8EC0FE);
   static const Color productLightSecondaryActive = Color(0xFF69ABFE);
+  static const Color errorRed = Color(0xFFF83448);
+
 
   //Normal
   static const Color productNormal = Color(0xFF4396FE);
